b61cf29e96f04ef5f683628deb4dd73aa95e48aa
jaykabu <59605424+jaykabu@users.noreply.github.com>
2020-12-24 18:07:23 +0530

    data store to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import {createStore , applyMiddleware} from 'redux';
 import logger from 'redux-logger';
+import {composeWithDevTools} from "redux-devtools-extension";
 import bookReducer from "./Book/BookReducer";
 
-const store = createStore(bookReducer, applyMiddleware(logger));
+const store = createStore(bookReducer,composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
